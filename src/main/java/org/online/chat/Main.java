928c632250a597ba233518815910763be828124c
Arsenii Holovanov <arsenijgolovanev@gmail.com>
2026-01-31 10:12:00 +0200

    Initial commit

---
+++ src/main/java/org/online/chat/Main.java
@@ -0,0 +1,32 @@
+package org.online.chat;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context ctx = tomcat.addContext("", new File("src/main/webapp/").getAbsolutePath());
+
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+                new DirResourceSet(resources, "/WEB-INF/classes",
+                        new File("target/classes").getAbsolutePath(), "/")
+        );
+        ctx.setResources(resources);
+
+        tomcat.enableNaming();
+        tomcat.getConnector();
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
