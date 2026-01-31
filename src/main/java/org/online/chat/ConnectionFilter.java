4bb079e554d5c96d7f4b085093d2a10f1b685e9b
Arsenii Holovanov <arsenijgolovanev@gmail.com>
2026-01-31 11:05:10 +0200

    Added ConnectionFilter

---
+++ src/main/java/org/online/chat/ConnectionFilter.java
@@ -0,0 +1,19 @@
+package org.online.chat;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class ConnectionFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setAttribute("conn", Main.conn);
+        chain.doFilter(request, response);
+    }
+}
--- src/main/java/org/online/chat/Main.java
+++ src/main/java/org/online/chat/Main.java
@@ -11,11 +11,21 @@ import java.io.File;
 import java.sql.*;
 
 public class Main {
+    public static Connection conn;
+
+    static {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:mydb.db");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
 
-        Context ctx = tomcat.addContext("", new File("src/main/webapp/").getAbsolutePath());
+        Context ctx = tomcat.addWebapp("", new File("src/main/webapp/").getAbsolutePath());
 
         WebResourceRoot resources = new StandardRoot(ctx);
         resources.addPreResources(
@@ -29,9 +39,5 @@ public class Main {
 
         tomcat.start();
         tomcat.getServer().await();
-    }
-
-    public static Connection createConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:sqlite:mydb.db");
     }
 }
