05756573f5a96c270695fd07c2f27c6d8c77f48b
Arsenii Holovanov <arsenijgolovanev@gmail.com>
2026-01-31 13:23:56 +0200

    Added doPost method implementation

--- src/main/java/org/online/chat/servlets/MessageServlet.java
+++ src/main/java/org/online/chat/servlets/MessageServlet.java
@@ -2,6 +2,7 @@ package org.online.chat.servlets;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.online.chat.models.Message;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -17,6 +19,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet({"/api/messages", "/api/messages/"})
 public class MessageServlet extends HttpServlet {
@@ -46,23 +49,31 @@ public class MessageServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            List<Message> messages = new ArrayList<>();
-            Connection conn = ((Connection) req.getAttribute("conn"));
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM messages");
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                messages.add(new Message(rs.getString("name"),
-                        rs.getString("message")));
+            Message message;
+            try (BufferedReader reader = req.getReader()) {
+                message = new Gson().fromJson(reader.lines().collect(Collectors.joining()), Message.class);
             }
+
+            Connection conn = ((Connection) req.getAttribute("conn"));
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO messages (name, message) VALUES (? ?)");
+            ps.setString(1, message.getName());
+            ps.setString(2, message.getMessage());
+            ps.executeUpdate();
 
             try (PrintWriter writer = resp.getWriter()) {
                 JsonObject result = new JsonObject();
-                result.addProperty("messages", new Gson().toJson(messages));
+                result.addProperty("status", "success");
                 writer.write(result.toString());
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } catch (JsonSyntaxException e) {
+            try (PrintWriter writer = resp.getWriter()) {
+                JsonObject result = new JsonObject();
+                result.addProperty("status", "error");
+                result.addProperty("message", "Incorrect format");
+                writer.write(result.toString());
+            }
         }
     }
 }
