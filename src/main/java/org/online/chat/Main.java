34846a47958292a9837f8b931e7f0eb39469591d
Arsenii Holovanov <arsenijgolovanev@gmail.com>
2026-02-04 12:27:49 +0200

    pfd 8

--- src/main/java/org/online/chat/Main.java
+++ src/main/java/org/online/chat/Main.java
@@ -1,12 +1,8 @@
 package org.online.chat;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 import java.sql.*;
@@ -28,14 +24,7 @@ public class Main {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.parseInt(System.getenv().getOrDefault("PORT", "8080")));
 
-        Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
-
-//        WebResourceRoot resources = new StandardRoot(ctx);
-//        resources.addPreResources(
-//                new DirResourceSet(resources, "/WEB-INF/classes",
-//                        new File("target/classes").getAbsolutePath(), "/")
-//        );
-//        ctx.setResources(resources);
+        tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
 
         tomcat.enableNaming(); // todo
         tomcat.getConnector().setProperty("address", "0.0.0.0");
