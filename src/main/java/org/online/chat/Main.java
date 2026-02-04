b71f792495b74aebe759ff2b0f292b4b27232dc7
Arsenii Holovanov <arsenijgolovanev@gmail.com>
2026-02-04 12:17:43 +0200

    pfd 7

--- src/main/java/org/online/chat/Main.java
+++ src/main/java/org/online/chat/Main.java
@@ -5,7 +5,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.JarResourceSet;
+import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
@@ -13,7 +13,11 @@ import java.sql.*;
 import java.util.TimeZone;
 
 public class Main {
-    private final static Dotenv dotenv = Dotenv.configure().systemProperties().ignoreIfMissing().load();
+    public final static Dotenv dotenv = Dotenv.configure().
+            ignoreIfMissing().
+            systemProperties().
+            load();
+
     private static Connection conn = null;
 
     static {
@@ -24,16 +28,16 @@ public class Main {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.parseInt(System.getenv().getOrDefault("PORT", "8080")));
 
-        Context ctx = tomcat.addWebapp("", new File(".").getAbsolutePath());
+        Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
 
-        WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addJarResources(
-                new JarResourceSet(resources, "/WEB-INF/classes",
-                        new File("target/online-chat-1.0-SNAPSHOT.jar").getAbsolutePath(), "/")
-        );
-        ctx.setResources(resources);
+//        WebResourceRoot resources = new StandardRoot(ctx);
+//        resources.addPreResources(
+//                new DirResourceSet(resources, "/WEB-INF/classes",
+//                        new File("target/classes").getAbsolutePath(), "/")
+//        );
+//        ctx.setResources(resources);
 
-        tomcat.enableNaming();
+        tomcat.enableNaming(); // todo
         tomcat.getConnector().setProperty("address", "0.0.0.0");
 
         tomcat.start();
